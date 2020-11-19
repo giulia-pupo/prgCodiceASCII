3e39caf93760b1b82743a714815b9c7d35829c67
Giulia <giuliapupo0@gmail.com>
2020-11-19 12:18:12 +0100

    Codice ASCII

--- prgCodiceASCII/Program.cs
+++ prgCodiceASCII/Program.cs
@@ -6,7 +6,17 @@ namespace prgCodiceASCII
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Inserici un valore numerico o alfanumerico da convertire in ASCII ");
+            char carattere = Convert.ToChar(Console.ReadLine());
+            int number = carattere;
+            //aggiunge 4 al numero decimale corrispondente della lettera inserita
+            //number =  number +4;
+     
+            Console.WriteLine($"Il carattere digitato corrisponde al numero: {number.ToString()}");
+            string binario = Convert.ToString(number, 2).PadLeft(8, 'o');
+
+            Console.WriteLine($"Il carattere in codice binaro corrisponde alla sequanza di bit {binario}");
+            Console.ReadKey();
         }
     }
 }
